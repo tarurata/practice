d35eb463a4392d4623c3b5814bd6efd3ce59dc8f
taru.rata <saycheese5963@yahoo.co.jp>
2018-06-18 01:12:07 +0900

    made create, insert and view functions in DB.

--- script2.py
+++ script2.py
@@ -1,12 +1,29 @@
 import sqlite3
 
-# connect to sqlite3
-conn = sqlite3.connect("lite.db")
-cur = conn.cursor()
+def create_table():
+    # connect to sqlite3
+    conn = sqlite3.connect("lite.db")
+    cur = conn.cursor()
+    # make cursor(This is like a pointer)
+    cur.execute("CREATE TABLE IF NOT EXISTS store (item TEXT, quantity INTEGER, price REAL)")
+    conn.commit()
+    conn.close()
 
-# make cursor(This is like a pointer)
-cur.execute("CREATE TABLE IF NOT EXISTS store (item TEXT, quantity INTEGER, price REAL)")
-cur.execute("INSERT INTO store VALUES ('Wine Glass', 8, 10.5)")
+def insert(item, quantity, price):
+    conn = sqlite3.connect("lite.db")
+    cur = conn.cursor()
+    cur.execute("INSERT INTO store VALUES (?, ?, ?)", (item, quantity, price))
+    conn.commit()
+    conn.close()
 
-conn.commit()
-conn.close()
+insert("Coffee Cup", 10, 5 )
+
+def view():
+    conn = sqlite3.connect("lite.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM store")
+    rows = cur.fetchall()
+    conn.close()
+    return rows
+
+print(view())
